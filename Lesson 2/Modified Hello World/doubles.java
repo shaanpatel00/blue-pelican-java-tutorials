64523e8649b1f609e072345b8102b7d1f47c04cb
shaanpatel00 <shaanpatel00@gmail.com>
2020-03-25 10:28:31 -0400

    Add files via upload

---
+++ Lesson 2/Modified Hello World/doubles.java
@@ -0,0 +1,9 @@
+
+public class doubles {
+	public static void main(String args[]) {
+		double d = -137.8036;
+		System.out.println(d);
+		
+		d = 1.45667E23;
+    }
+}
---
+++ Lesson 2/Modified Hello World/ints.java
@@ -0,0 +1,7 @@
+
+public class ints {
+	public static void main(String args[]) {
+		int age = 59;
+		System.out.println(age);
+    }
+}
